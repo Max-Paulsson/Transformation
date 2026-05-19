eb956a871c54ce2687928e25f5310eaca53cea76
max paulsson <maxpaulsson@icloud.com>
2026-05-19 23:58:53 +0200

    Finished transfor.py and started on scan.py

--- transform.py
+++ transform.py
@@ -29,3 +29,18 @@ def four_point_transformation(image, pts):
     WidthA = np.sqrt(((br[0] - bl[0]) **2) + ((br[1] - bl[1]) **2))
     WidthB = np.sqrt(((tr[0] - tl[0]) **2) + ((tr[1] - tl[1]) **2))
     maxWidth = max(int(WidthA), int(WidthB))
+
+    heightA = np.sqrt(((tr[0] - br[0]) **2) + ((tr[1] - br[1]) **2))
+    heightB = np.sqrt(((tl[0] - bl[0]) **2) + ((tl[1] - bl[1]) **2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    dst = np.array([
+        [0, 0],
+        [maxWidth -1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight -1]], dtype= "float32")
+    
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    return warped
